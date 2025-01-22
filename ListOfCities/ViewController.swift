65e36b79ff85aea4a5617c03352d40dd5c106f73
bemadeveloper <akbermet1990@icloud.com>
2025-01-22 23:50:59 +0500

    Initial Commit

---
+++ ListOfCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListOfCities
+//
+//  Created by Bema on 22/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
